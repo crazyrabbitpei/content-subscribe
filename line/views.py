629589fb71cd67ff54b07a218a59c4982c7ca428
Your Name <you@example.com>
2020-11-23 19:24:23 +0800

    first test

---
+++ line/views.py
@@ -0,0 +1,49 @@
+from django.shortcuts import render
+from django.http import HttpResponse, JsonResponse, HttpResponseBadRequest, HttpResponseForbidden
+
+import logging
+logger = logging.getLogger(__name__)
+
+import os
+import time
+from linebot import (
+    LineBotApi, WebhookHandler
+)
+from linebot.exceptions import (
+    InvalidSignatureError, LineBotApiError
+)
+from linebot.models import (
+    MessageEvent, TextMessage, TextSendMessage,
+)
+
+line_bot_api = LineBotApi(os.getenv('CHANNEL_ACCESS_TOKEN'))
+handler = WebhookHandler(os.getenv('CHANNEL_SECRET'))
+# Create your views here.
+
+def callback(request):
+    if request.method != 'POST':
+        return HttpResponseBadRequest()
+
+    # get X-Line-Signature header value
+    signature = request.META['X-Line-Signature']
+
+    # get request body as text
+    body = request.body.decode('utf-8')
+    logger.info(f"Request body: {body}")
+    # handle webhook body
+    try:
+        handler.handle(body, signature)
+    except InvalidSignatureError:
+        logger.error("Invalid signature. Please check your channel access token/channel secret.")
+        return HttpResponseForbidden()
+    except LineBotApiError:
+        logger.error("Line bot api error")
+        return HttpResponseBadRequest()
+
+    return HttpResponse()
+
+@handler.add(MessageEvent, message=TextMessage)
+def handle_message(event):
+    line_bot_api.reply_message(
+        event.reply_token,
+        TextSendMessage(text=event.message.text))
