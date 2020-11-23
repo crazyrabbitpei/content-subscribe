005950f543fdf70ca2088ea45015e4aa45196d48
Your Name <you@example.com>
2020-11-23 21:10:39 +0800

    test url

--- line/urls.py
+++ line/urls.py
@@ -2,5 +2,6 @@ from django.urls import path, include
 from . import views
 
 urlpatterns = [
-    path('callback/', views.callback)
+    path('callback/', views.callback),
+    path('test/', views.test),
 ]
