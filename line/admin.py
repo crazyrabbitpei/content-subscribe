aeab49447982d4aea948c715b0c59f59eccfc531
Your Name <you@example.com>
2020-11-26 17:04:43 +0800

    操作介面

--- line/admin.py
+++ line/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
+from .models import User, Keyword
 
 # Register your models here.
+class KeywordInline(admin.TabularInline):
+    model = Keyword.user_id.through
+
+@admin.register(User)
+class UserAdmin(admin.ModelAdmin):
+    list_display = ('user_id', 'follow_date', 'status')
+    inlines = [KeywordInline]
+
+@admin.register(Keyword)
+class KeywordAdmin(admin.ModelAdmin):
+    list_display = ('keyword', 'create_time')
+
