b008c49c5d052035078fd236feba50cf04980b67
Your Name <you@example.com>
2020-11-26 17:19:02 +0800

    回覆字串包成gettext_lazy

--- line/views.py
+++ line/views.py
@@ -136,7 +136,7 @@ def echo(event):
     try:
         line_bot_api.reply_message(
             event.reply_token,
-            TextSendMessage(text=message))
+            TextSendMessage(text=f'{message}'))
     except LineBotApiError as e:
         etype, value, tb = sys.exc_info()
         logger.error(f'Reply api error {etype}', exc_info=True)
