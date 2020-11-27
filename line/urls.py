d3d65b112026f41e89fed364263e1bdcfb92cc0f
Your Name <you@example.com>
2020-11-27 15:48:04 +0800

    url namespace and name

--- line/urls.py
+++ line/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path, include
 from . import views
 
+app_name = 'line'
 urlpatterns = [
-    path('callback/', views.callback),
-    path('test/', views.push_notice),
+    path('callback/', views.callback, name='callback'),
+    path('test/', views.push_notice, name='test'),
 ]
