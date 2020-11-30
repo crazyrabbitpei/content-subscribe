3b0944d657c5fc5d9cfabb7bd539f0645e4d86d2
Your Name <you@example.com>
2020-11-30 20:01:36 +0800

    set expire time and set ttl

--- line/tool/cache.py
+++ line/tool/cache.py
@@ -2,51 +2,78 @@ import redis
 import os
 from collections import defaultdict
 
-KEYWORD_TMP = defaultdict(list)
+GLOBAL_KEYWORD_TTL = 60*60*24*5
+USER_INFO_TTL = 60*60*24*7
+USER_TYPING_KETWORDS_TTL = 60*60
+
 pool = redis.ConnectionPool.from_url(os.getenv('REDIS_URL'), decode_responses=True)
+
 
 def get_user_info(user_id):
     '''
-    {state, follow_date, user_id}
+    {state, user_id}
     '''
     r = redis.StrictRedis(connection_pool=pool)
-    return r.hgetall(f'user_info:{user_id}')
+    user_info = r.hgetall(f'user_info:{user_id}')
+    refresh_user_info_ttl(r, user_id)
+    return user_info
 
 def update_user_info(user_id, user_info):
     r = redis.StrictRedis(connection_pool=pool)
+    refresh_user_info_ttl(r, user_id)
     r.hset(f'user_info:{user_id}', mapping=user_info)
 
 def update_state(user_id, new_state):
     r = redis.StrictRedis(connection_pool=pool)
+    refresh_user_info_ttl(r, user_id)
     return r.hset(f'user_info:{user_id}', 'state', new_state)
+
+def refresh_user_info_ttl(r, user_id):
+    return r.expire(f'user_info:{user_id}', USER_INFO_TTL)
 
 def add_tmp_keyword(user_id, keyword):
     r = redis.StrictRedis(connection_pool=pool)
+    refresh_tmp_keywords_ttl(r, user_id)
     return r.sadd(f'user_typing_keywords:{user_id}', keyword)
 
 def delete_tmp_keywords(user_id, keywords):
     r = redis.StrictRedis(connection_pool=pool)
+    refresh_tmp_keywords_ttl(r, user_id)
     return r.srem(f'user_typing_keywords:{user_id}', *keywords)
 
 def get_tmp_keywords(user_id):
     r = redis.StrictRedis(connection_pool=pool)
+    refresh_tmp_keywords_ttl(r, user_id)
     return list(r.smembers(f'user_typing_keywords:{user_id}'))
+
+def refresh_tmp_keywords_ttl(r, user_id):
+    return r.expire(f'user_typing_keywords:{user_id}', USER_TYPING_KETWORDS_TTL)
 
 def add_global_keyword(keyword, last_user_id):
     r = redis.StrictRedis(connection_pool=pool)
-    return r.set(f'keyword:{keyword}', last_user_id, ex=60*60*24*5, keepttl=True)
+    refresh_global_keyword_ttl(r, keyword)
+    return r.set(f'keyword:{keyword}', last_user_id)
 
 def get_global_keyword(keyword):
     r = redis.StrictRedis(connection_pool=pool)
+    refresh_global_keyword_ttl(r, keyword)
     return r.get(f'keyword:{keyword}')
+
+def refresh_global_keyword_ttl(r, keyword):
+    return r.expire(f'keyword:{keyword}', GLOBAL_KEYWORD_TTL)
 
 def update_user_keywords(user_id, keywords):
     r = redis.StrictRedis(connection_pool=pool)
+    refresh_user_keywords_ttl(r, user_id)
     return r.sadd(f'user_keywords:{user_id}', *keywords)
 
 def get_user_keywords(user_id):
     r = redis.StrictRedis(connection_pool=pool)
+    refresh_user_keywords_ttl(r, user_id)
     return list(r.smembers(f'user_keywords:{user_id}'))
+
+def refresh_user_keywords_ttl(r, user_id):
+    return r.expire(f'user_keywords:{user_id}', USER_INFO_TTL)
 
 def delete_user(user_id):
     r = redis.StrictRedis(connection_pool=pool)
