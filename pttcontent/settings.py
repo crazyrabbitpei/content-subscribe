39b4b26d2a4538c38c1fe0b5fbcc61084281026a
Your Name <you@example.com>
2020-12-23 10:32:12 +0800

    sensitive

--- pttcontent/settings.py
+++ pttcontent/settings.py
@@ -24,8 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '5z*r_7ehb3t&dd(xw)r2@1tyc3a-gkwyoujc*l1&o$anwxhb&*'
-APPEND_SLASH = False
+SECRET_KEY = os.getenv('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.getenv('DEBUG') == "True"
 ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS').split(',')
